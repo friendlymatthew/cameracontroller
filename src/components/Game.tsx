f2b814c58051b7e81045e8ab32fc83f25169810b
Matthew <38759997+friendlymatthew@users.noreply.github.com>
2023-11-25 20:39:55 -0500

    added shadow

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -38,48 +38,50 @@ export default function Game() {
 
     const renderer = rendererRef.current;
 
-    const scene = new THREE.Scene();
-
     const camera = new THREE.PerspectiveCamera(
       45,
       window.innerWidth / window.innerHeight,
       1,
       100
     );
-    camera.position.set(0, 2, -10);
+    camera.position.set(1, 2, -3);
     camera.lookAt(0, 1, 0);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xa0a0a0);
+    scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
+
+    const hemiLight = new THREE.HemisphereLight(0xffffff, 0x8d8d8d, 3);
+    hemiLight.position.set(0, 20, 0);
+    scene.add(hemiLight);
+
+    const dirLight = new THREE.DirectionalLight(0xffffff, 3);
+    dirLight.position.set(-3, 10, -10);
+    dirLight.castShadow = true;
+    dirLight.shadow.camera.top = 2;
+    dirLight.shadow.camera.bottom = -2;
+    dirLight.shadow.camera.left = -2;
+    dirLight.shadow.camera.right = 2;
+    dirLight.shadow.camera.near = 0.1;
+    dirLight.shadow.camera.far = 40;
+    scene.add(dirLight);
+
+    const mesh = new THREE.Mesh(
+      new THREE.PlaneGeometry(100, 100),
+      new THREE.MeshPhongMaterial({ color: 0xcbcbcb, depthWrite: false })
+    );
+    mesh.rotation.x = -Math.PI / 2;
+    mesh.receiveShadow = true;
+    scene.add(mesh);
 
     const controls = new OrbitControls(camera, renderer.domElement);
     controls.enableDamping = true;
     controls.dampingFactor = 0.05;
 
-    const planeGeometry = new THREE.PlaneGeometry(100, 100);
-    const planeMaterial = new THREE.MeshPhongMaterial({
-      color: "#ffffff",
-      shininess: 200,
-    }); // Green, for example
-    const plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = -Math.PI / 2;
-    plane.position.y = 0;
-    scene.add(plane);
-
-    const axesHelper = new THREE.AxesHelper(5);
-    scene.add(axesHelper);
-
-    const gridHelper = new THREE.GridHelper(10, 10);
-    scene.add(gridHelper);
-
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     renderer.shadowMap.enabled = true;
     mountRef.current.appendChild(renderer.domElement);
-
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
-    scene.add(ambientLight);
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLight.position.set(5, 5, 5);
-    scene.add(directionalLight);
 
     const loader = new GLTFLoader();
 
@@ -88,6 +90,12 @@ export default function Game() {
 
       modelRef.current = gltf.scene;
       scene.add(modelRef.current);
+
+      modelRef.current.traverse((object) => {
+        if (object instanceof THREE.Mesh) {
+          object.castShadow = true; 
+        }
+      });
 
       const mixer = new THREE.AnimationMixer(modelRef.current);
       mixerRef.current = mixer;
@@ -130,10 +138,6 @@ export default function Game() {
       requestAnimationFrame(animate);
       const delta = clock.getDelta();
       mixerRef.current?.update(delta);
-
-      if (gameAction === ACTION.RUN && modelRef.current) {
-        plane.position.z += 0.1 * delta;
-      }
 
       renderer.render(scene, camera);
     };
